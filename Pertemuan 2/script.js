2211df80c11d20315c2349de35761ed1d986fb3b
Ahmad Kurniawan <ahmaadkurniawaan@gmail.com>
2023-11-29 20:05:16 +0700

    initial commit

---
+++ Pertemuan 2/script.js
@@ -0,0 +1,7 @@
+ // Fungsi untuk mengubah teks pada elemen dengan ID "judul" dan "paragraf"
+ function ubahTeks() {
+    let nama = "Ahmad Kurniawan";
+    document.getElementById("nama").innerHTML = nama;
+    document.getElementById("judul").innerHTML = "Halo, JavaScript!";
+    document.getElementById("paragraf").innerHTML = "Teks diubah oleh JavaScript.";
+}
